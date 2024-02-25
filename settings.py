a52cdeb895ecba129de2816728a32bd830002d84
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-02-25 13:22:57 +0300

    feat: add basic settings for project to run

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///yacut.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
