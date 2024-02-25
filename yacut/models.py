820a2fb91202773a821c650d796e94e8689443e6
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-02-25 20:01:48 +0300

    feat: add default value for timestamp

--- yacut/models.py
+++ yacut/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime as dt
+
 from . import db
 
 
@@ -5,7 +7,7 @@ class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(256), unique=True, nullable=False)
     short = db.Column(db.String(16), unique=True, nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=dt.now())
 
     def to_dict(self):
         return dict(
