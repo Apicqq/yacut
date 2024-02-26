06f8be4a2d703373b4d2d005e0d16864f8bf1700
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-02-26 19:16:02 +0300

    refactor: update original field for URLMap model to be non-unique

--- yacut/models.py
+++ yacut/models.py
@@ -5,6 +5,6 @@ from . import db
 
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    original = db.Column(db.String(256), unique=True, nullable=False)
+    original = db.Column(db.String(256), nullable=False)
     short = db.Column(db.String(16), unique=True, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False, default=dt.now())
