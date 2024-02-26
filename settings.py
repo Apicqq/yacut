1104414b55db72bb7142ee41644f71eb0b9449a2
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-02-26 18:22:26 +0300

    chore: add default DB

--- settings.py
+++ settings.py
@@ -5,5 +5,6 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY", "you-will-never-guess")
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URI",
-        "sqlite:///db.sqlite3")
+        "sqlite:///db.sqlite3"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
