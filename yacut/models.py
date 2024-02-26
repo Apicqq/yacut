d1d0eae839c234afea50bc975df18a646c84ca39
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-02-26 19:17:15 +0300

    refactor: update timestamp field to correctly output time

--- yacut/models.py
+++ yacut/models.py
@@ -7,4 +7,4 @@ class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(256), nullable=False)
     short = db.Column(db.String(16), unique=True, nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False, default=dt.now())
+    timestamp = db.Column(db.DateTime, nullable=False, default=dt.utcnow)
